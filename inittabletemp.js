a0eb123e55dbc5f802522275b5725de9fb2ed639
mikilukasik <szarhazi@gmail.com>
2015-06-29 10:25:16 +0100

    .

--- inittabletemp.js
+++ inittabletemp.js
@@ -1,88 +1,58 @@
 var Dbtable=function (tableNum) {	//class
 	this.tableNum = tableNum,
-	this.table=[],
+	
 	this.wNext=true,
 	this.aiOn=false,
 	this.chat=[],
 	this.moves=[],
 	this.pollNum=1,
 	this.allPastTables=[]
-	console.log('dskhbbvkdbfksdbc')
-}
-
-function initTable(tNo){
-		aiOn[tNo]=false
-		allPastTables[tNo]=[]
-		
-//randomConst[tNo]=5//Math.random()*100
-						//if(Math.random()>0.5){randomConst[tNo]=1/randomConst[tNo]}
-
 	
-	pollNum[tNo]=1
-
-//function initTable(){	
-	allWNexts[tNo]=true
-	allChats[tNo]=[]
-	allMoves[tNo]=[]
-	//var tempString=""							
-	//var 
-	allTables[tNo] = new Array(8)							//create 8x8 array
+	this.table = new Array(8)							//create 8x8 array
 	for (var i = 0; i < 8; i++) {
-		allTables[tNo][i] = new Array(8)
+		this.table[i] = new Array(8)
 	}
 	
 
 
-	for(j=2; j<6; j++){ 							//make the blanks blank
+	for(var j=2; j<6; j++){ 							//make the blanks blank
 		for(i=0; i<8; i++){
-			allTables[tNo][i][j]=[0,0,false,false,false]//,blankFunction]		
-			//[][]=[color,piece,selected,isInItsOriginalPosition for king and rook or CanBeHitEnPass for pawns,highLighted,canMoveTo]
+			this.table[i][j]=[0,0,false,false,false]	
 		}
 	}
-
-
-
-
-	//wNext=true
-
-
-
-	// [3] is isInItsOriginalPosition for king and rook or CanBeHitEnPass for pawns
 	
+			// [3] is isInItsOriginalPosition for king and rook or CanBeHitEnPass for pawns
 	for (var i = 0; i < 8; i++) {									//row of white pawns
 		
-		allTables[tNo][i][1]=[2,1,false,false,false]//,pawnCanMove]
+		this.table[i][1]=[2,1,false,false,false]//,pawnCanMove]
 	}
 	for (var i = 0; i < 8; i++) {									//row of black pawns
-		allTables[tNo][i][6]=[1,1,false,false,false]//,pawnCanMove]
+		this.table[i][6]=[1,1,false,false,false]//,pawnCanMove]
 	}
-	allTables[tNo][0][0]=[2,4,false,true,false]//,rookCanMove]				//rooks
-	allTables[tNo][7][0]=[2,4,false,true,false]//,rookCanMove]
-	allTables[tNo][0][7]=[1,4,false,true,false]//,rookCanMove]
-	allTables[tNo][7][7]=[1,4,false,true,false]//,rookCanMove]
+	this.table[0][0]=[2,4,false,true,false]//,rookCanMove]				//rooks
+	this.table[7][0]=[2,4,false,true,false]//,rookCanMove]
+	this.table[0][7]=[1,4,false,true,false]//,rookCanMove]
+	this.table[7][7]=[1,4,false,true,false]//,rookCanMove]
 
-	allTables[tNo][1][0]=[2,3,false,true,false]//,horseCanMove]					//knights
-	allTables[tNo][6][0]=[2,3,false,true,false]//,horseCanMove]
-	allTables[tNo][1][7]=[1,3,false,true,false]//,horseCanMove]
-	allTables[tNo][6][7]=[1,3,false,true,false]//,horseCanMove]
+	this.table[1][0]=[2,3,false,true,false]//,horseCanMove]					//knights
+	this.table[6][0]=[2,3,false,true,false]//,horseCanMove]
+	this.table[1][7]=[1,3,false,true,false]//,horseCanMove]
+	this.table[6][7]=[1,3,false,true,false]//,horseCanMove]
 	
-	allTables[tNo][2][0]=[2,2,false,true,false]//,bishopCanMove]				//bishops
-	allTables[tNo][5][0]=[2,2,false,true,false]//,bishopCanMove]
-	allTables[tNo][2][7]=[1,2,false,true,false]//,bishopCanMove]
-	allTables[tNo][5][7]=[1,2,false,true,false]//,bishopCanMove]
+	this.table[2][0]=[2,2,false,true,false]//,bishopCanMove]				//bishops
+	this.table[5][0]=[2,2,false,true,false]//,bishopCanMove]
+	this.table[2][7]=[1,2,false,true,false]//,bishopCanMove]
+	this.table[5][7]=[1,2,false,true,false]//,bishopCanMove]
 
-	allTables[tNo][3][0]=[2,5,false,true,false]//,queenCanMove]				//w queen
-	allTables[tNo][4][0]=[2,9,false,true,false]//,kingCanMove]				//w king
+	this.table[3][0]=[2,5,false,true,false]//,queenCanMove]				//w queen
+	this.table[4][0]=[2,9,false,true,false]//,kingCanMove]				//w king
 	
-	allTables[tNo][3][7]=[1,5,false,true,false]//,queenCanMove]				//b q
-	allTables[tNo][4][7]=[1,9,false,true,false]//,kingCanMove]				//b k
+	this.table[3][7]=[1,5,false,true,false]//,queenCanMove]				//b q
+	this.table[4][7]=[1,9,false,true,false]//,kingCanMove]				//b k
 	
-	//console.log("initTable done")
-	
-//}
-  console.log(allTables[tNo])
+
   
-  allTables[tNo]=addMovesToTable(allTables[tNo],true)
-  protectPieces(allTables[tNo],true)
-  protectPieces(allTables[tNo],false)
+  // allTables[tNo]=addMovesToTable(allTables[tNo],true)
+  // protectPieces(allTables[tNo],true)
+  // protectPieces(allTables[tNo],false)
 }
