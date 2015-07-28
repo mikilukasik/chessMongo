a908feae602c17c5dbc0cb5cff6109f1b9cd5ef5
Miklos Lukasik <szarhazi@gmail.com>
2015-07-28 21:14:53 +0100

    .

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -131,7 +131,7 @@ function captured(table,color){
 						
 						console.log('give true')
 						returnAid=true
-						return null;
+						return ('a');
 					}
 				})
 				//do stuff and exit loop
