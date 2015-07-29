6c2f21f32c1e1f688d54d9d108864ec8ab7eff79
Miklos Lukasik <szarhazi@gmail.com>
2015-07-29 21:44:21 +0100

    beauty

--- aiserver.js
+++ aiserver.js
@@ -1,7 +1,4 @@
-
 //app.listen(16789
-
-
 var express = require('express');
 var morgan = require('morgan');
 var mongodb = require('mongodb');
@@ -12,63 +9,68 @@ var app = express();
 //app.use(express.static('public'))
 app.use(morgan("combined"))
 
-var cn='mongodb://localhost:17890/chessdb'
+var cn = 'mongodb://localhost:17890/chessdb'
 
-eval(fs.readFileSync('public/brandNewAi.js')+'');
+eval(fs.readFileSync('public/brandNewAi.js') + '');
 //eval(fs.readFileSync('public/tableClass.js')+'');
 
+app.get('/aiChoice', function(req, res) {
 
-app.get('/aiChoice', function (req, res) {
+	mongodb.connect(cn, function(err, db) {
+		db.collection("tables").findOne({
+			tableNum: Number(req.query.t)
+		}, function(err2, tableFromDb) {
 
-  mongodb.connect(cn, function(err, db) {
-    db.collection("tables").findOne({tableNum: Number(req.query.t)},function(err2, tableFromDb) {
-    
-      if(!(tableFromDb==null)){
-       
-        var result=ai(tableFromDb.table,tableFromDb.wNext)
-        
-        if(result.length>1){  //if there are any moves
-          
-          var aiMoveString=result[1][0]// the winning movestring
-          
-          var toConsole=[]  //to chat?
-          toConsole[0]=result[0][2]//timeItTook
-          
-          for(var i=1;i<result.length;i++){
-            toConsole[i]=               result[i][0]+'__'+
-                                          result[i][1]+'__'+
-                                          result[i][2]+'__'+
-                                          result[i][3];
-                                             
-          }
-          
-          
-          
-          var sendJson={aimove: aiMoveString, fulltable: result, toconsole: toConsole}
-        }else{
-          var sendJson={aimove: "", fulltable: result}
-        }
-      }else{
-        var sendJson={error:"error"}
-      }
-      
-      db.close()
-      res.json(sendJson);
-    
-    });
-  });
+			if(!(tableFromDb == null)) {
+
+				var result = ai(tableFromDb.table, tableFromDb.wNext)
+
+				if(result.length > 1) { //if there are any moves
+
+					var aiMoveString = result[1][0] // the winning movestring
+
+					var toConsole = [] //to chat?
+
+					toConsole[0] = result[0][2] //timeItTook
+
+					for(var i = 1; i < result.length; i++) {
+
+						toConsole[i] = result[i][0] + '__' +
+							result[i][1] + '__' +
+							result[i][2] + '__' +
+							result[i][3];
+
+					}
+
+					var sendJson = {
+						aimove: aiMoveString,
+						fulltable: result,
+						toconsole: toConsole
+					}
+				} else {
+					var sendJson = {
+						aimove: "",
+						fulltable: result
+					}
+				}
+			} else {
+				var sendJson = {
+					error: "error"
+				}
+			}
+
+			db.close()
+			res.json(sendJson);
+
+		});
+	});
 });
 
+var server = app.listen(16789, function() {
 
+	var host = server.address().address;
+	var port = server.address().port;
 
-
-
-
-var server = app.listen(16789, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('aiserver is listening at http://%s:%s', host, port);
 
 });
