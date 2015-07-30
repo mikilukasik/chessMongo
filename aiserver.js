5bf61953a911a559f1b336af5157f97785aca941
Miklos Lukasik <szarhazi@gmail.com>
2015-07-30 20:21:27 +0100

    try

--- aiserver.js
+++ aiserver.js
@@ -23,7 +23,7 @@ app.get('/aiChoice', function(req, res) {
 
 			if(!(tableFromDb == null)) {
 
-				var result = ai(tableFromDb.table, tableFromDb.wNext)
+				var result = ai(tableFromDb.table, tableFromDb.wNext)			//		ai	<------------
 
 				if(result.length > 1) { //if there are any moves
 
@@ -64,6 +64,58 @@ app.get('/aiChoice', function(req, res) {
 
 		});
 	});
+});
+
+app.get('/speedTest', function(req, res) {
+
+	var result = []
+
+	var testDepth = 5	//default
+	if(!isNaN(req.query.d)) testDepth = req.query.d	//or set
+
+	var testTable = moveIt("e2e4", new Dbtable("test", "test", "test"))
+	testTable.wNext = false
+
+	for(var i = 0; i < testDepth; i++) {
+
+		result[i] = ai(testTable, tableFromDb.wNext) //		ai	<------------
+
+	}
+
+	var toConsole = [] //to chat?
+	var avgSpeed=0
+	
+	for (var i=0; i<result.length; i++){
+		avgSpeed += result[i][0][2]
+	}
+	
+	avgSpeed /=result.length
+	
+	toConsole[0] = 'Average: '+avgSpeed+' ms.  Move: ' + result[0][1][0] // the winning movestring
+
+	for(var i = 0; i < result.length - 1; i++) {
+
+		toConsole[i + 1] = (i + 1) + ': ' + result[i][0][2] + ' ms' +
+
+	}
+
+	var sendJson = {
+
+		toconsole: toConsole
+		
+	}
+
+	res.json(sendJson);
+
+});
+
+var server = app.listen(16789, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('aiserver is listening at http://%s:%s', host, port);
+
 });
 
 var server = app.listen(16789, function() {
