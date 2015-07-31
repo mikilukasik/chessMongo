3ed236823b88b1deb0fd58eccd5b0c015e62e1b8
mikilukasik <szarhazi@gmail.com>
2015-07-31 20:58:52 +0100

    gg

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -915,19 +915,19 @@ function createAiTable(cfTable, cfColor, dontDoScnd) {
 			tTable2Value /= 1000
 		
 		
-			var opponentsBestValue2=createAiTable(tempTable, !cfColor,true)
+			var opponentsBestValue=createAiTable(tempTable, !cfColor,true)[1][1]
 			//[1][1]/10000
 			// if (opponentsBestValue2.length==1){
 			// 	opponentsBestValue=-100000 
 			// }else{
 					
-			if(opponentsBestValue2.length==1){
-				var opponentsBestValue=-10000
+			// if(opponentsBestValue2.length==1){
+			// 	var opponentsBestValue=-10000
 				
-			}else{
-				var opponentsBestValue=opponentsBestValue2[1][1]
-			}
-			//}
+			// }else{
+			// 	var opponentsBestValue=opponentsBestValue2[1][1]
+			// }
+			// //}
 		}
 
 		
