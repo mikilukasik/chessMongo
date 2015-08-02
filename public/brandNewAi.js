7f7103b03ea33e2c0bd8dbfda8bb5c5e73456a3f
Miklos Lukasik <szarhazi@gmail.com>
2015-08-02 03:02:04 +0100

    .

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -911,7 +911,7 @@ function createAiTable(cfTable, cfColor, oppDontDoScnd) {
 
 			//var arrLen = opponentsBestValue2.length
 			//opponentsBestValue = -10000 / Math.pow(10001, arrLen - 1)
-			if(origHisMoveCount == 0 //|| twoStepsToWin 
+			if(opponentsBestValue2.length == 1 || twoStepsToWin 
 				 ) {
 				if (captured(tempTable,!cfColor)){
 					opponentsBestValue= -10000
