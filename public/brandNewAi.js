581aa76d3990bdacc2a35b78f9a6d9daf7011712
Miklos Lukasik <szarhazi@gmail.com>
2015-08-04 21:40:38 +0100

    return here

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -890,7 +890,7 @@ function createAiTable(cfTable, cfColor, skipScnd) {
 		//var tTableValue=0
 		
 		//temp ignore fwd
-		var fwdVal = (stepMove[1]-stepMove[3])*0.001//4 // mennyit megy elore
+		var fwdVal = 0//(stepMove[1]-stepMove[3])*0.001//4 // mennyit megy elore
 		
 		//protectTable(tempTable)
 
