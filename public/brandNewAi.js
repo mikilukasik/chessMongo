db88fae96b53bd328693668cd81d0a03a15e4972
Miklos Lukasik <szarhazi@gmail.com>
2015-08-05 08:30:50 +0100

    na most

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -942,8 +942,8 @@ function createAiTable(cfTable, cfColor, skipScnd) {
 			var tempRetData = getTableData(tempRetTable, cfColor)
 			
 			var retTableValue = tempRetData[0] 
-			var retMyHitValue = tempRetData[1]
-			var retHisHitValue = tempRetData[2]
+			var retMyHitValue = tempRetData[1]*10
+			var retHisHitValue = tempRetData[2]/10
 			
 			if (retTableValue+retMyHitValue-retHisHitValue<tempRetValue){
 				tempRetValue=retTableValue+retMyHitValue-retHisHitValue
