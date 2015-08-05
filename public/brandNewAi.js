cb8f7367bd4073d07e451952b3e6c16df21eed86
Miklos Lukasik <szarhazi@gmail.com>
2015-08-05 11:03:58 +0100

    .

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -967,7 +967,7 @@ function createAiTable(cfTable, cfColor, skipScnd) {
 			var rtnMyHitValue = retData[1]
 			var rtnHisHitValue = retData[2]
 			
-			var rtnValue=(retHitValue-fHitValue)*100-(rtnHisHitValue-origHisHitValue)*10+(rtnMyHitValue-origMyHitValue)*100//+(rtnTableValue - origTableValue)*1.1 //+ (rtnMyHitValue - origMyHitValue ) - (rtnHisHitValue - origHisHitValue)//(scndHitValue - origHitValue) +* 10.01
+			var rtnValue=(fHitValue-retHitValue)*100-(rtnHisHitValue-origHisHitValue)*10+(rtnMyHitValue-origMyHitValue)*100//+(rtnTableValue - origTableValue)*1.1 //+ (rtnMyHitValue - origMyHitValue ) - (rtnHisHitValue - origHisHitValue)//(scndHitValue - origHitValue) +* 10.01
 		
 		}
 
