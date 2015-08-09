34003f99849aa652dfa275564a3da507eafda405
mikilukasik <szarhazi@gmail.com>
2015-08-09 17:57:17 +0100

    empty fields for sanc

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -787,11 +787,24 @@ function getTableData(origTable, isWhite) { //, rtnSimpleValue) {
 		if(origTable[0][0][3])tableValue+=.01	//unmoved rook worth more than moved
 		if(origTable[7][0][3])tableValue+=.01
 		
+		if(origTable[6][0][3])tableValue-=.01
+		if(origTable[5][0][3])tableValue-=.01
+		if(origTable[3][0][3])tableValue-=.01
+		if(origTable[2][0][3])tableValue-=.01
+		if(origTable[1][0][3])tableValue-=.01
+		
+		
 	}
 	
 	if(!isWhite&&origTable[4][7][3]){	//we play with white and have not moved the king yet
 		if(origTable[0][7][3])tableValue+=.01	//unmoved rook worth more than moved
 		if(origTable[7][7][3])tableValue+=.01
+		
+		if(origTable[6][7][3])tableValue-=.01
+		if(origTable[5][7][3])tableValue-=.01
+		if(origTable[3][7][3])tableValue-=.01
+		if(origTable[2][7][3])tableValue-=.01
+		if(origTable[1][7][3])tableValue-=.01
 		
 	}
 
