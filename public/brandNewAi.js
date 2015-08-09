14df822e7b764af0ab32067804cc646ac6360c34
mikilukasik <szarhazi@gmail.com>
2015-08-09 17:13:12 +0100

    .

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -1004,7 +1004,8 @@ function createAiTable(cfTable, cfColor, skipScnd) {
 
 			rtnValue = loopValue + mhit + hhit + retProtect//my hit matters most as i'm next
 			if(cfColor){
-				if(stepMove=='e1g1'||stepMove=='e1c1')rtnValue+=.01
+				if(stepMove=='e1g1'||stepMove=='e1c1')rtnValue+=.01	//sancoljon ha tud
+				
 			}else{
 				if(stepMove=='e8g8'||stepMove=='e8c8')rtnValue+=.01
 			}
