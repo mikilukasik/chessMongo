5d80a7c11872e53076175353ec64e341e060885a
Miklos Lukasik <szarhazi@gmail.com>
2015-08-13 01:32:32 +0100

    nem csak paraszt takarhatja kiralyt sanc elott

--- public/brandNewAi.js
+++ public/brandNewAi.js
@@ -1308,14 +1308,14 @@ function createAiTable(cfTable, cfColor, skipScnd) {
 			//rtnValue = loopValue + mhit + hhit + retProtect//my hit matters most as i'm next
 			
 			if(cfColor){
-				if((stepMove=='e1g1'&&cfTable[5][1][1]==1&&cfTable[6][1][1]==1&&cfTable[7][1][1]==1)
+				if((stepMove=='e1g1'&&cfTable[5][1][0]==2&&cfTable[6][1][0]==2&&cfTable[7][1][0]==2)	//vegig covered
 					||
-					(stepMove=='e1c1'&&cfTable[0][1][1]==1&&cfTable[1][1][1]==1&&cfTable[2][1][1]==1))sancValue+=.35	//sancoljon ha jol esik
+					(stepMove=='e1c1'&&cfTable[0][1][0]==2&&cfTable[1][1][0]==2&&cfTable[2][1][0]==2))sancValue+=.35	//sancoljon ha jol esik
 				
 			}else{
-				if((stepMove=='e8g8'&&cfTable[5][6][1]==1&&cfTable[6][6][1]==1&&cfTable[7][6][1]==1)
+				if((stepMove=='e8g8'&&cfTable[5][6][0]==1&&cfTable[6][6][0]==1&&cfTable[7][6][0]==1)
 					||
-					(stepMove=='e8c8'&&cfTable[0][6][1]==1&&cfTable[1][6][1]==1&&cfTable[2][6][1]==1))sancValue+=.35	//sancoljon ha jol esik
+					(stepMove=='e8c8'&&cfTable[0][6][0]==1&&cfTable[1][6][0]==1&&cfTable[2][6][0]==1))sancValue+=.35	//sancoljon ha jol esik
 				
 			}
 				
