ae56f8f71f2a9107046ed50d9bebdb5083f44a75
Miklos Lukasik <szarhazi@gmail.com>
2015-08-15 22:14:32 +0100

    and again the same

--- server.js
+++ server.js
@@ -434,23 +434,10 @@ app.get('/chat', function(req, res) {
 								// console.log(resJsn)
 								// console.log('dssdfsdgs')
 								if(!(resJsn == null || tableInDb == null)) {
-									//var moveStr = String(resJsn.aimove)
-									//if(!(moveStr=="")){   			//there's at least 1 move
-									// var toPush = String(tableInDb.table[dletters.indexOf(moveStr[0])][moveStr[1] - 1][0]) + //color of whats moving
-									// 	tableInDb.table[dletters.indexOf(moveStr[0])][moveStr[1] - 1][1] + //piece
-									// 	moveStr + //the string
-									// 	tableInDb.table[dletters.indexOf(moveStr[2])][moveStr[3] - 1][0] + //color of whats hit
-									// 	tableInDb.table[dletters.indexOf(moveStr[2])][moveStr[3] - 1][1] //piece
 
-									// if(!(toPush==tableInDb.moves[tableInDb.moves.length-1])){
-									//tableInDb.moves.push(toPush)
-									//tableInDb.table = moveIt(moveStr, tableInDb.table)
-									//tableInDb.wNext = !tableInDb.wNext
 									tableInDb.pollNum++
 										//tableInDb.moved = new Date().getTime()
 										tableInDb.chat.push(resJsn.toconsole)
-
-									//tableInDb.table = addMovesToTable(tableInDb.table, tableInDb.wNext)
 
 									db.collection("tables")
 										.save(tableInDb, function(err3, res) {})
