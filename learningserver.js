f43a669edf6266d927fdfa9a49fafc34a3cfab24
Miklos Lukasik <szarhazi@gmail.com>
2015-10-01 00:59:03 +0100

    .

--- learningserver.js
+++ learningserver.js
@@ -172,7 +172,7 @@ setInterval(function() {
 															path: '/move?t=' + tableInDb.tableNum+'&m='+moveStr
 														};
 														
-														http.request(options2, function(response,db2,tableInDb){
+														http.request(options2, function(response,db2){
 															//itt kene visszaneveni a learnert
 															tableInDb.wName="learner"
 															db2.collection("tables")									
