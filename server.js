d8fc3dd5b19cd2fc89ea2fae09b5f4c7e878b3f9
Miklos Lukasik <szarhazi@gmail.com>
2015-10-08 05:49:56 +0100

    .

--- server.js
+++ server.js
@@ -1315,7 +1315,7 @@ app.get('/stats.txt', function(req, res) {
 										asyncHack--	//request answered
 										if(asyncHack==0){
 											res.write('end')
-											res.end()
+											//res.end()
 										}
 									})
 												
