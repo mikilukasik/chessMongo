33f9d8e87f3032447f1c1307885ca75664b0bd3f
Miklos Lukasik <szarhazi@gmail.com>
2015-10-09 07:09:53 +0100

    .

--- server.js
+++ server.js
@@ -1100,7 +1100,7 @@ app.get('/whoIsLearning', function(req, res) {
 	for (var i=0;i<learners[0].length;i++){
 		res.write(learners[0][i]+String.fromCharCode(9)+learners[2][i]+String.fromCharCode(9)+learners[4][i]+String.fromCharCode(9)+learners[6][i]+String.fromCharCode(9)+learners[5][i]+String.fromCharCode(13))
 	}
-	res.end()
+	//res.end()
 })
 
 app.get('/learnerPoll', function(req, res) {
