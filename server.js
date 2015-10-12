68cc7095048d8994980cfda9551e0afac9538580
Miklos Lukasik <szarhazi@gmail.com>
2015-10-12 17:24:44 +0100

    .

--- server.js
+++ server.js
@@ -336,7 +336,7 @@ function ping(msecs){
 	for (var i=pendingThinkerPolls.length-1;i>-1;i--){
 		if(pendingThinkerPolls[i][2] < new Date().getTime()-msecs){
 			//polled more tham MSECS time ago, let's pop it
-			sendTask(pendingThinkerPolls[i][0].qury.id,'ping','ping')
+			sendTask(pendingThinkerPolls[i][0].query.id,'ping','ping')
 			
 		}
 	}
