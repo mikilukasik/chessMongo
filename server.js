f94d0c776670fddafb14254af0f9198ad3fcbf9c
Miklos Lukasik <szarhazi@gmail.com>
2015-10-14 00:36:14 +0100

    .

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ var evalToClient=function(){
 			// aiOn:false,
 			// toBeChecked:true
 			
-			gameToEval.toBeChecked=false
+			toBeChecked=false
 			
 		},function(errx,gameToEval){
 			//send gameToEval to fastest available client
