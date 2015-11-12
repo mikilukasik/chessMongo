228fd2117efef692fb9641457ae572829a9ab464
Miklos Lukasik <szarhazi@gmail.com>
2015-11-12 01:30:42 +0000

    .

--- public/engine.js
+++ public/engine.js
@@ -565,7 +565,7 @@ function getHitScores(origTable, isWhite) {
 		
 	//}
 
-	return myBestHit+(myAllHit-hisAllHit-hisBestHit+myprotectScore-hisprotectScore)/64//-(hisBestHit/16)+(myprotectScore[0]-(hisprotectScore[0]+hisAllHit)/16+myAllHit)/256//,myBestHitCoords] //, hisTempPieces, rtnMyHitSum[0], rtnHisHitSum[0], rtnMyMovesCount] //returnArray // elso elem az osszes babu ertekenek osszge, aztan babkuk
+	return myBestHit+(myAllHit-hisAllHit-hisBestHit+myprotectScore-hisprotectScore)/1024//-(hisBestHit/16)+(myprotectScore[0]-(hisprotectScore[0]+hisAllHit)/16+myAllHit)/256//,myBestHitCoords] //, hisTempPieces, rtnMyHitSum[0], rtnHisHitSum[0], rtnMyMovesCount] //returnArray // elso elem az osszes babu ertekenek osszge, aztan babkuk
 
 }
 
