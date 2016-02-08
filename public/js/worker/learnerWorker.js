11aed7f5be31f0540bcfe4f7a597f0b1e5b1b018
mikilukasik <szarhazi@gmail.com>
2016-02-08 04:44:08 +0000

    .

--- public/js/worker/learnerWorker.js
+++ public/js/worker/learnerWorker.js
@@ -4,15 +4,21 @@ importScripts('../all/brandNewAi.js')
 importScripts('../worker/deepening.js')
 importScripts('../index/httpreq.js')
 
-var myID=''
+var learnerGlobals={}
+//     myID:''
+// }
 
 var playModGamePair=function(mod){
     mod.modVal=mod.min+(mod.max-mod.min)*Math.random()
     console.log('Starting modded gamePair:',mod)
+    
+    
+    
+    
 }
 
 var play=function(){
-    simpleGet('/api/mod/type?id='+myID,function(ret){
+    simpleGet('/api/mod/type?id='+learnerGlobals.myID,function(ret){
         
         var resp=JSON.parse( ret.response)
         
@@ -43,7 +49,7 @@ onmessage = function(event){
             
             console.log('starting learner')
             
-            myID=event.data.myID
+            learnerGlobals.myID=event.data.myID
             play()
             
             //sockets(event.data.host)
