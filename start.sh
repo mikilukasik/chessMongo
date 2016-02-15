0705ae9daa6409ac8e6e190c3a3f735762a2ec58
mikilukasik <szarhazi@gmail.com>
2016-02-15 06:50:28 +0000

    .

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash  
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 
+cd chessMongo
+
+
+
+
+mongod --port 17890 --fork --syslog --smallfiles --dbpath data
+
 nohup nodemon server.js </dev/null
